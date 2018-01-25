a83bcb861b76d0582e1ae36c0a8061a06842bb47
milosrs <milosribar@yahoo.com>
2018-01-25 23:21:55 +0100

    Paket "app" je sada main.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from Tracker.HandTracker import HandTracker
-import cv2
+from View.MainWindow import *
 
 
 def main():
     tracker = HandTracker(2)
     tracker.printVersions()
-    tracker = cv2.TrackerGOTURN_create();
-
+    capture_thread = threading.Thread(target=grab, args = (0, q, 1280, 720, 30))
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
