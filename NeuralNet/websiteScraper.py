0f6a10a7466892c6231e8d50caa28f79bfc8fa73
milosrs <milosribar@yahoo.com>
2018-02-03 16:19:19 +0100

    Scrape google images.

---
+++ NeuralNet/websiteScraper.py
@@ -0,0 +1,6 @@
+import os
+import urllib.request as urlib
+from bs4 import BeautifulSoup as Soup
+import json
+
+url1 = "https://www.google.rs/search?rlz=1C1AVNE_enRS679RS679&tbm=isch&sa=1&ei=AdJ1WrrNMcyusAGAopuIAg&q=hand&oq=hand&gs_l=psy-ab.3..0i67k1l7j0l3.136398.136619.0.136841.4.3.0.0.0.0.200.340.0j1j1.2.0....0...1c.1.64.psy-ab..2.2.334....0.H7Zq11Rr5rY"
